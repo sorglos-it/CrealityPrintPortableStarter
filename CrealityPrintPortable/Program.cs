c3f964d67a9f007641cfdc664e418c15d3edea55
Sorglos Thomas Weirich <info@sorglos.org>
2026-06-29 19:10:37 +0200

    push

---
+++ CrealityPrintPortable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrealityPrintPortable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process ExternalProcess = new Process();
+            ExternalProcess.StartInfo.FileName = "cmd.exe";
+            ExternalProcess.StartInfo.Arguments = "/c start CrealityPrint.exe --datadir profile";
+            ExternalProcess.StartInfo.UseShellExecute = false;
+            ExternalProcess.Start();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# CrealityPrintPortableStarter
+# CrealityPrintPortable
+Executable for running CrealityPrint in portable mode
+Profile settings will be saved in `profile` folder
 
+## Installation
+- Download and unzip CrealityPrint: [https://github.com/CrealityOfficial/CrealityPrint/releases](https://github.com/SoftFever/CrealityPrint)
+- Copy `CrealityPrintPortableStarter.exe` inside your CrealityPrint folder (next to the original `Creality-print.exe`)
+- Run `CrealityPrintPortableStarter.exe`
+
+
+copy of https://github.com/entrhopi/PrusaPrintPortable. and recoded for crealityprint. 
